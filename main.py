89e1774f89ba430ed58ab01208f22958857686f6
Ignatius-Reilly <76879455+Ignatius-Reilly@users.noreply.github.com>
2026-04-22 20:30:39 +0000

    Pretende agregar lógica de DBs a los endpoints
    Pero no funciona bien...

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def iniciar_db():
     conn = sqlite.connect("letterbox.db")
     cursor = conn.cursor()
     cursor.execute("""
-        CREATE TABLE IF NOT EXIST peliculas (
+        CREATE TABLE IF NOT EXISTS peliculas (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         titulo TEXT,
         genero TEXT,
@@ -20,15 +20,24 @@ app = FastAPI()
 
 @app.get("/peliculas")
 def obtener_películas():
-    return db_peliculas
+    conn = sqlite3.connect("letterbox.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM peliculas")
+    res = [dict(row) for row in cursor.fetchall()]
+    conn.close()
+    return res
 
 @app.post("/peliculas")
 def cargar_una_pelicula(titulo, genero, puntaje):
     if not puntaje.isdigit():
         mensaje_de_retorno = "el puntaje tiene que ser un número entero"
         return mensaje_de_retorno
-
-    nueva_pelicula = {"titulo": titulo, "genero": genero, "puntaje": puntaje}
-    db_peliculas.append(nueva_pelicula)
+    conn = sqlite.connect("letterbox.db")
+    cursor = conn.cursor()
+    cursor.execute("""
+        INSERT INTO peliculas (titulo, genero, puntaje)
+    """)
+    conn.commit()
+    conn.close()
     mensaje_de_retorno = "Película guardada correctamente"
     return mensaje_de_retorno
